0c2ae3d8d35e1a3f3aa52f82a727286cc2c89d6d
Thien Chu <chubtgcs18576@fpt.edu.vn>
2024-03-04 09:50:43 +0700

    setting

--- src/routers/uesrsRouter.js
+++ src/routers/uesrsRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const UsersContronller = require("../contronllers/UsersContronller");
 
+router.post("/register", UsersContronller.register);
 router.get("/", UsersContronller.index);
 
 module.exports = router;
