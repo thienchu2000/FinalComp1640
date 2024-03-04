ae13b9dee90f079e6a0c05c65bd33b26f0e0d6ac
Thien Chu <chubtgcs18576@fpt.edu.vn>
2024-03-04 17:25:11 +0700

    jwt

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,12 @@
 const homeRouter = require("./homeRouter");
 const uesrsRouter = require("./uesrsRouter");
 const newsRouter = require("./newsRouter");
+const {
+  isAdmin,
+  isStaff,
+  isStudent,
+  isTeacher,
+} = require("../utils/authorize");
 
 function router(app) {
   app.use("/news", newsRouter);
--- src/utils/authorize.js
+++ src/utils/authorize.js
@@ -5,4 +5,28 @@ function isAdmin(req, res, next) {
     return res.send("ban khong duoc phep");
   }
 }
-module.exports = { isAdmin };
+
+function isStaff(req, res, next) {
+  if (res.user.role === "staff") {
+    next();
+  } else {
+    return res.send("ban khong duoc");
+  }
+}
+
+function isStudent(req, res, next) {
+  if (res.user.role === "student") {
+    next();
+  } else {
+    return res.send("ban khong duoc");
+  }
+}
+
+function isTeacher(req, res, next) {
+  if (res.user.role === "teacher") {
+    next();
+  } else {
+    return res.send("ban khong duoc");
+  }
+}
+module.exports = { isAdmin, isStaff, isStudent, isTeacher };
