a667a9c4773268724bc2a04061f6f3f18a79f29f
Thien Chu <chubtgcs18576@fpt.edu.vn>
2024-03-13 15:50:54 +0700

    pull

--- src/controllers/ArticlesController.js
+++ src/controllers/ArticlesController.js
@@ -7,36 +7,21 @@ const { GoogleAuth } = require("google-auth-library");
 const { myOAuth2Client } = require("../config/email");
 const Articles = require("../models/Articles");
 const Facultis = require("../models/Facultis");
+const multer = require("multer");
 
 class ArticlesController {
   index(req, res, next) {
     res.send("hello world");
   }
 
-  // CRUD---ASSIGNMENT.
+  // CRUD---articles
 
   // send --- ASS database send email teacher.
   async createAss(req, res, next) {
-    const { submitsions, teacherId, studentId } = req.body;
+    const { articlesName, studentId } = req.body;
     console.log(req.body);
 
     try {
-      if (!submitsions) {
-        return res.status(400).send("Please send a submission");
-      }
-      // const assignments = new Assignments({
-      //   submitsions,
-      // });
-      // assignments.save();
-      // res.status(200).send("Done");
-      const checkdb = await Users.findById({ _id: teacherId, _id: studentId })
-        .populate("subject")
-        .populate("role");
-      console.log(checkdb);
-      // const check = checkdb
-      //   .filter((item) => item.role.name === "Teacher")
-      //   .map((item) => item.email);
-      // console.log(check);
     } catch (err) {
       return res.status(500).send("err db");
     }
